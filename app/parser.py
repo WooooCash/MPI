5a172a111f5238406f1ebb493d2d51789bd6c620
lbialczak <lbialczak@grupazpr.pl>
2024-01-05 19:08:26 +0100

    parse obj into quadtree

--- app/parser.py
+++ app/parser.py
@@ -1,7 +1,19 @@
+import math
+
 import pywavefront
-import quads
+from quads import QuadTree
 
 
-def parse(filename: str):
+def parse(filename: str) -> QuadTree:
     scene = pywavefront.Wavefront(filename)
-    quad_tree = quads.QuadTree(0, 0 ,0)
+    min_x = min(vertex[0] for vertex in scene.vertices)
+    max_x = min(vertex[0] for vertex in scene.vertices)
+    min_y = min(vertex[1] for vertex in scene.vertices)
+    max_y = min(vertex[1] for vertex in scene.vertices)
+
+    quad_tree = QuadTree((0, 0), math.ceil(max_x - min_x), math.ceil(max_y - min_y))
+
+    for vertex in scene.vertices:
+        quad_tree.insert((vertex[0], vertex[1]))
+
+    return quad_tree
